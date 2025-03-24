b20635b9a7d997e1f5307c51eb918d60943499a7
eleekku <144835604+eleekku@users.noreply.github.com>
2025-03-24 14:37:31 +0200

    remove js file

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-// Simple TypeScript code
-window.onload = function () {
-    var message = "Welcome to the Pong game!";
-    var header = document.createElement("h2");
-    header.textContent = message;
-    document.body.appendChild(header);
-};
