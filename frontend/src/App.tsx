8b96e20fa8950af7e1f34ea493cf82eb5b0b8b90
eleekku <144835604+eleekku@users.noreply.github.com>
2025-03-27 12:29:01 +0200

    frontend using react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,10 @@ import React from "react";
 
 const App: React.FC = () => {
     return (
-        <div>
-            {/* Header */}
-            <h2>Welcome to the Pong game!</h2>
-
-            {/* Login Form */}
-            <LoginForm />
-        </div>
-    );
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to Pong</h1>
+      </div>);
 };
 
-const LoginForm: React.FC = () => {
-    return (
-        <form
-            style={{
-                position: "absolute",
-                right: "50px",
-                top: "50px",
-            }}
-        >
-            <input
-                type="text"
-                placeholder="Username"
-                style={{ marginRight: "10px" }}
-            />
-            <button type="submit">Login</button>
-        </form>
-    );
-};
 
 export default App;
