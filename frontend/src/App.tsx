10ce3ba2088f9f3f3cd871c3dc95add0e0e6e4eb
Deivy Zurita <deivylex93@gmail.com>
2025-03-28 14:24:44 +0200

    adding the game code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState }  from "react";
+import Pong from "./Pong";
 
 const App: React.FC = () => {
-    return (
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Welcome to Pong</h1>
-      </div>);
+  const [leftScore, setLeftScore] = useState(0);
+  const [rightScore, setRightScore] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Pong</h1>
+      <h2>Score: {leftScore} - {rightScore}</h2>
+      <Pong setLeftScore={setLeftScore} setRightScore={setRightScore} />
+    </div>
+  );
 };
 
 
