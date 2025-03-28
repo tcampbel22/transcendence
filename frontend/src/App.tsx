0addded407ce06eb5c86b68ead5a262eea4693b7
eleekku <144835604+eleekku@users.noreply.github.com>
2025-03-28 14:07:20 +0200

    User log in form, game mode selection, react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import login from "./components/Login";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App: React.FC = () => {
     return (
+      <Routes>
+        <Route path="/" element={
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         <h1>Welcome to Pong</h1>
         <p>Choose if you want to play 1v1 or tournament</p>
         <button>1v1</button>
         <button>versus AI</button>
         <button>Tournament</button>
-      </div>
+        <Login />
+      </div>} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     );
 };
 
