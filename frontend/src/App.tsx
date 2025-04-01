a4aa225277c73a3e40d1b34d9666f944b26c9c2a
Timothy Campbell <tcampbel@student.hive.fi>
2025-04-01 17:10:19 +0300

    Added tournament logic and re renders page for tournaments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useState }  from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Pong from "./Pong";
-import Matchmaking from "./components/Matchmaking";
+import Tournament from "./components/Tournament";
+import OneVsOnePage from "./pages/OneVsOnePage";
+import TournamentPage from "./pages/TournamentPage";
 
 
 const App: React.FC = () => {
-	const [scores, setScores] = useState({ left: 0, right: 0 });
+	
 
     return (
       <Routes>
@@ -15,14 +16,17 @@ const App: React.FC = () => {
       <div className="flex flex-col items-center gap-4 my-4">
         <h1>Welcome to Pong</h1>
         <p>Choose if you want to play 1v1 or tournament</p>
-			<Matchmaking />
-			<button className="bg-red-500 hover:bg-red-200 text-white font-bold py-2 px-2 rounded-lg shadow-md transition">1v1</button>
-			<button className="bg-green-500 hover:bg-green-200 text-white font-bold py-2 px-2 rounded-lg shadow-md transition">1vAI</button>
+			<Link to="/play/1v1">
+            <button className="bg-red-500 hover:bg-red-200 text-white font-bold py-2 px-2 rounded-lg shadow-md transition">1v1</button>
+          </Link>
+		  <Link to="/play/Tournament">
+			<button className="bg-green-500 hover:bg-green-200 text-white font-bold py-2 px-2 rounded-lg shadow-md transition">Tournament</button>
+		  </Link>
 			<Login />
-        	<h2>Score: {scores.left} - {scores.right}</h2>
-        <Pong onScoreChange={(left, right) => setScores({ left, right })} />
       </div>} />
         <Route path="/register" element={<Register />} />
+		<Route path="/play/1v1" element={<OneVsOnePage />} />
+		<Route path="/play/Tournament" element={<TournamentPage />} />
       </Routes>
     );
 };
