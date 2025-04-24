d0d54809d41b9de08809b40007ed2538a2d9ac33
xriera-c <149150079+xrierac@users.noreply.github.com>
2025-04-24 13:21:17 +0300

    Add SSL certificates for microservices

--- backend/user_service/index.js
+++ backend/user_service/index.js
@@ -1,18 +1,27 @@
 import Fastify from "fastify";
 import { testConnection } from "./database/db.js";
-import  loginRoutes  from "./api/routes/login.routes.js"
+import loginRoutes from "./api/routes/login.routes.js";
 import registerRoutes from "./api/routes/register.routes.js";
 import profileRoutes from "./api/routes/profile.routes.js";
-import logger from "@eleekku/logger"
-import cors from '@fastify/cors';
+import logger from "@eleekku/logger";
+import cors from "@fastify/cors";
+import fs from "fs";
 
-const fastify = Fastify({ logger: true });
+const SSL_CERT_PATH = "./ssl/cert.pem";
+const SSL_KEY_PATH = "./ssl/key.pem";
 
+const fastify = Fastify({
+	logger: true,
+	https: {
+		key: fs.readFileSync(SSL_KEY_PATH),
+		cert: fs.readFileSync(SSL_CERT_PATH),
+	},
+});
 //testing purposes only to get frontend connected in the dev env.
 fastify.register(cors, {
 	origin: ["http://localhost:5173"], // 👈 Vite's default dev server port
-	method: ["GET", "POST", "PUT", "DELETE"]
-  });
+	method: ["GET", "POST", "PUT", "DELETE"],
+});
 
 try {
 	fastify.register(loginRoutes);
@@ -28,10 +37,10 @@ const start = async () => {
 	try {
 		const dbConnected = await testConnection();
 		if (!dbConnected) {
-			logger.error('user_service failed to connect to the user database')
-		  throw new Error('Failed to connect to the user database');
+			logger.error("user_service failed to connect to the user database");
+			throw new Error("Failed to connect to the user database");
 		}
-		logger.info('user_service connected to the user database')
+		logger.info("user_service connected to the user database");
 		await fastify.listen({ port: 3002, host: "0.0.0.0" });
 	} catch (err) {
 		fastify.log.error(err);
