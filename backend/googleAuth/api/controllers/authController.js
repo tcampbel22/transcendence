2a04a93c5b7d83fe6db91c64e8d566f77e68ca7a
deivy <deivylex93@gmail.com>
2025-04-29 23:09:15 +0300

    fixed https issue in googleAuth

--- backend/googleAuth/api/controllers/authController.js
+++ backend/googleAuth/api/controllers/authController.js
@@ -3,34 +3,29 @@
  If the registration succeeds, it redirects to a frontend page with userID(/hub). If the user is already registered (409 error) just redirect to frontend with userId(/hub),
  For other errors, it returns the error status code in the redirect URL.*/
 
-
  import axios from 'axios';
  import https from 'https';
  
  export const googleCallback = async (req, reply) => {
      const profile = req.user;
- 
      try {
          const payload = {
              username: profile.displayName,
              email: profile.emails[0].value,
              password: profile.id,
          };
- 
          const httpsAgent = new https.Agent({
              rejectUnauthorized: false,
          });
- 
          const response = await axios.post(
              `https://nginx:4433/users/register`,
              payload,
              { httpsAgent }
-         );
- 
+         ); 
          const userData = encodeURIComponent(JSON.stringify({ userId: response.data.userId }));
          reply.redirect(`/auth/google/callback.html?user=${userData}`);
      } 
-     catch (error) {
+     catch (error) { 
          if (error.response && error.response.status === 409) {
              const userData = encodeURIComponent(JSON.stringify({ userId: error.response.data.userId }));
              reply.redirect(`/auth/google/callback.html?user=${userData}`);
