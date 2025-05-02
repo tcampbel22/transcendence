814476682ca4b2ec333cc4eb9a7b4d2222fdb4b5
Timothy Campbell <tcampbel@student.hive.fi>
2025-05-02 17:08:36 +0300

    try tests again

--- backend/tests/jest.config.js
+++ backend/tests/jest.config.js
@@ -1,11 +1,10 @@
 export default {
-	testEnvironment: 'node',
-	transform: {},
-	extensionsToTreatAsEsm: [".js"], // Treat .js files as ES modules
-	moduleNameMapper: {
-	  "^(\\.{1,2}/.*)\\.js$": "$1", // Fix imports by removing .js extensions
-	},
-	transformIgnorePatterns: [
-		"/node_modules/(?!@error-lib)/", // Replace with the library name(s) you need to transform
-	  ],
-  };
+  testEnvironment: 'node',
+  transform: {}, // Disable default Jest transformations for ES modules
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Fix imports by removing .js extensions
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@error-lib)/', // Replace with the library name(s) you need to transform
+  ],
+};
