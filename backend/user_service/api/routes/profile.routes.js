e1bbed7fdfaa99e0e20c5020731a94f25181fca6
Timothy Campbell <tcampbel@student.hive.fi>
2025-05-12 15:59:46 +0300

    Commented out jwt, so dev works

--- backend/user_service/api/routes/profile.routes.js
+++ backend/user_service/api/routes/profile.routes.js
@@ -3,7 +3,7 @@ import * as schemas from "../schemas/profile.schema.js"
 import authenticate from "../../../libs/jwt_authenticator/jwt_authenticator.js";
 
 export default async function profileRoutes(fastify, options) {
-	fastify.addHook("preHandler", authenticate); //UNCOMMENT THIS LINE TO ENABLE AUTHENTICATION
+	// fastify.addHook("preHandler", authenticate); //UNCOMMENT THIS LINE TO ENABLE AUTHENTICATION
     
 	// Check if user exists
     fastify.get("/api/validate/:id", { schema: schemas.validateUserSchema }, profileController.validateUser);
