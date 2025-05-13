4a6a6a9f3b6c00723b25f3aa06bb7f130e51b3ee
Timothy Campbell <tcampbel@student.hive.fi>
2025-05-13 13:59:12 +0300

    Removed commented out code in register controller

--- backend/user_service/api/controllers/register.controller.js
+++ backend/user_service/api/controllers/register.controller.js
@@ -1,54 +1,12 @@
 import { registerService } from "../services/register.service.js";
-import { normalize } from "../utils/normalize.js";
 import { checkForExistingUser } from "../utils/checkForExisting.js";
 import { prisma } from "../../database/db.js";
 import logger from "@eleekku/logger";
-import fs from 'fs';
-import path from 'path';
-import { pipeline } from 'stream';
-import util from 'util';
 
 export const registerController = {
 
 	async registerUser(request, reply) {
 		try {
-			// const contentType = request.headers['content-type'] || '';
-			// let userData;
-			// let pictureUrl = '/uploads/default.png'; // Default image URL
-			
-			// if (contentType.includes('multipart/form-data')) {
-			// 	// Handle file upload case
-			// 	const data = await request.file();
-			// 	if (!data)
-			// 		throw new ErrorNotFound(`No file uploaded`);
-				
-			// 	userData = {};
-			// 	for await (const field of data.fields) {
-			// 		userData[field.fieldname] = field.value;
-			// 		console.log(`Field in pic: ${field}`);
-			// 	}
-			// 	if (!userData)
-			// 		throw new ErrorUnAuthorized(`File is corrupted`);
-			// 	// Process uploaded image if provided
-			// 	if (data.file) {
-			// 	const fileExtension = path.extname(data.filename).toLowerCase();
-			// 	if (!['.jpg', '.jpeg', '.png'].includes(fileExtension)) {
-			// 		throw new ErrorUnAuthorized(`File should be jpg, jpeg or png`);
-			// 	}
-				
-			// 	const filename = `user_new_${Date.now()}${fileExtension}`;
-			// 	const uploadDir = process.env.NODE_ENV === 'dev' ? './uploads' : '/app/uploads';
-			// 	const filepath = `${uploadDir}/${filename}`;
-			// 	pictureUrl = `/uploads/${filename}`;
-				
-			// 	const pump = util.promisify(pipeline);
-			// 	await pump(data.file, fs.createWriteStream(filepath));
-			// 	}
-			// } else {
-			// 	userData = request.body;
-			// 	pictureUrl = userData.picture || pictureUrl;
-			// }
-
 			const { username, email, password } = request.body;
 			if (password.toLowerCase().includes(username.toLowerCase())) {
 				logger.info(`User registration failed: ${username} tried to use username in password`);
