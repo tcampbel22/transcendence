40d8d66cf093e6f0a2e8147523194beb0a06f236
eleekku <144835604+eleekku@users.noreply.github.com>
2025-05-29 18:05:44 +0300

    debuginh prod for friendlist

--- frontend/src/hooks/useStartGame.tsx
+++ frontend/src/hooks/useStartGame.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import axios from "axios";
+import api from "../lib/api";
 
 type StartGameProps = {
   isGameStarted: boolean;
@@ -14,9 +15,11 @@ export const useStartGame = ({ isGameStarted, userId, opponentUserId, setGameId 
       const startGame = async () => {
         try {
           const API_URL = import.meta.env.VITE_API_GAME;
-          const response = await axios.post(`${API_URL}/create-game`, {
+          const response = await api.post(`${API_URL}/create-game`, {
             player1Id: userId,
             player2Id: opponentUserId,
+          }, {
+            withCredentials: true,
           });
           console.log("Game started successfully:", response.data);
           setGameId(response.data.gameId); 
